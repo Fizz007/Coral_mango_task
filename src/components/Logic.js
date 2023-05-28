edb373e6207af1fa5355f786bd3fe93d285f38d8
Faisal Abrar <erfaisalabrar@gmail.com>
2023-05-28 11:57:36 +0530

    ES6 basics added

---
+++ src/components/Logic.js
@@ -0,0 +1,26 @@
+//  const a = [1, 3, 5, 2, 4];
+
+// const result1 = a.filter((item) => {
+//     return item === 1 || item === 5 || item === 4;
+// });
+
+// console.log(result1);
+
+// solution  2==> const a = [1, 3, 5, 2, 4];
+
+// const result2 = a.map((item) => {
+//   return item * item ;
+// });
+
+// console.log(result2);
+
+// solution  3==> const a = [1, 3, 5, 2, 4];
+
+// const result3 = a.filter((item) => {
+//     return item === 1 || item === 5 || item === 4;
+// }).map((item) => {
+//   return item * item;
+// });
+
+// console.log(result3);
+
